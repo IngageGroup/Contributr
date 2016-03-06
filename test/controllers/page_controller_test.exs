6cf7beef97b8bb58e094a5422d4e30baf6712379
Matt Silbernagel <silbermm@gmail.com>
2016-03-06 14:39:38 -0500

    Fixed tests

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule Contributr.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "CONTRIBUTR"
   end
 end

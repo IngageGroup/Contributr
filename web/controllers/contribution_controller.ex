545e29f292ec8172216db87cc33143d91d26d274
Charlie Retzler <charlie.retzler@gmail.com>
2016-11-12 00:53:42 -0500

    Centralized logic for filtering drop down list

--- web/controllers/contribution_controller.ex
+++ web/controllers/contribution_controller.ex
@@ -16,12 +16,8 @@ require Logger
 
   def new(conn,  %{"organization" => organization}) do
     changeset = Contribution.changeset(%Contribution{})
-    eligible = Repo.all(  
-      from u in Contributr.User,
-      #where: u.eligible_to_recieve == true,
-      select: {u.name, u.id}    
-    )
-    render(conn, "new.html", changeset: changeset, organization: organization, eligible_users: eligible)
+    eligible_users = eligible_users(conn)
+    render(conn, "new.html", changeset: changeset, organization: organization, eligible_users: eligible_users)
   end
 
   def create(conn, %{"organization" => organization, "contribution" => contribution_params}) do
@@ -35,13 +31,8 @@ require Logger
         |> put_flash(:info, "Contribution created successfully.")
         |> redirect(to: contribution_path(conn, :index, organization))
       {:error, changeset} ->
-        eligible = Repo.all(  
-         from u in Contributr.User,
-         #where: u.eligible_to_recieve == true,
-         select: {u.name, u.id}
-        )
-
-        render(conn, "new.html", changeset: changeset, organization: organization, eligible_users: eligible)
+        eligible_users = eligible_users(conn)
+        render(conn, "new.html", changeset: changeset, organization: organization, eligible_users: eligible_users)
     end
   end
 
@@ -49,38 +40,18 @@ require Logger
     contribution = Repo.get!(Contribution, id)
     |> Repo.preload(:to_user)
  
-    eligible = eligible_users(conn)
-    render(conn, "show.html", contribution: contribution, organization: organization, eligible_users: eligible)
+    eligible_users = eligible_users(conn)
+    render(conn, "show.html", contribution: contribution, organization: organization, eligible_users: eligible_users)
   end
 
   def edit(conn, %{"organization" => organization, "id" => id}) do
     contribution = Repo.get!(Contribution, id)
     |> Repo.preload(:to_user)
 
-    eligible = Repo.all(
-      from u in Contributr.User,
-      where: u.eligible_to_recieve == true,
-      select: {u.name, u.id}
-    )
-
-    #eligible = Repo.all(User)  
-    #|> Enum.map(&{&1.name, &1.id})
-
-    #  from u in Contributr.User,
-      #where: u.eligible_to_recieve == true,
-    #  select: u
-    #)
-
-    #eligible2 = eligible |> Enum.Map(fn {k, v} -> Logger.info "#{k} = #{v}" end)
-    Logger.info "THIS IS ELIGIBLE #{inspect eligible}"
-
-    #Logger.info "THIS IS USERS #{inspect eligible.length}"
-
-    #eligible = eligible_users(conn)
-    #Logger.info "#{eligible.length}"
+    eligible_users = eligible_users(conn)
 
     changeset = Contribution.changeset(contribution)
-    render(conn, "edit.html", contribution: contribution, organization: organization, eligible_users: eligible, changeset: changeset)
+    render(conn, "edit.html", contribution: contribution, organization: organization, eligible_users: eligible_users, changeset: changeset)
   end
 
   def update(conn, %{"organization" => organization, "id" => id, "contribution" => contribution_params}) do
@@ -98,8 +69,8 @@ require Logger
         |> put_flash(:info, "Contribution updated successfully.")
         |> redirect(to: contribution_path(conn, :show, organization, contribution))
       {:error, changeset} ->
-        eligible = eligible_users(conn)
-        render(conn, "edit.html", contribution: contribution, organization: organization, eligible_users: eligible, changeset: changeset)
+        eligible_users = eligible_users(conn)
+        render(conn, "edit.html", contribution: contribution, organization: organization, eligible_users: eligible_users, changeset: changeset)
     end
   end
 
@@ -111,19 +82,16 @@ require Logger
     conn
     |> put_flash(:info, "Contribution deleted successfully.")
     |> redirect(to: contribution_path(conn, :index, organization))
-    end
-
+  end
 
   def eligible_users(conn) do
     user = current_user(conn)
 
     Repo.all(  
       from u in Contributr.User,
-      where: u.eligible_to_recieve == true and u.from_user_id != ^user.id,
-      select: u
+      where: u.eligible_to_recieve == true and u.id != ^user.id,
+      select: {u.name, u.id}
     )
-
-    #
   end
 
   def current_user(conn) do

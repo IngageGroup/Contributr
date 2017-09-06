6f60f67a8079b2f1c265cd1e90d6c8b1420b4a69
Kevin Hamon <kevin.hamon@ingagepartners.com>
2017-09-06 13:37:01 -0400

    Adding docker, compose and convienence script for developers.

---
+++ startContributr.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+#Replace with google client ID and secret for your organization.
+export GCLIENT_ID=<your google client ID>
+export GSECRET=<your google client ID>
+
+
+start Contributr containers daemonized.
+docker-compose up -d
+
